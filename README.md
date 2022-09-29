a5be9e248c75f13d454ffd2febf31354c061bdcc
Rui Luo <luo.rui@husky.neu.edu>
2022-09-29 13:26:12 -0400

    added commandline support

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ A simple program to capture from webcam like device and render it in SDL.
 Inspired from https://github.com/marcosps/v4l2_webcam
 
 ## Build (video streaming)
-**Before building, change the `dev_name` in `video_audio_proc/src/v4l_sdl.c` to the actual path of the device you want to capture.**  
-
 
 1. `cd video_audio_proc && mkdir build && cd build`  
 2. `cmake .. && make`
 
 The commands will generate an executable `v4l_sdl_stream` in the `build` folder.
 
-Run it view the video streaming from the camera.
+## Usage
+Run `v4l_sdl_stream -d [device-name]` or just `v4l_sdl_stream` to capture from the default device.
 
 
 
--- video_audio_proc/src/v4l_sdl.c
+++ video_audio_proc/src/v4l_sdl.c
@@ -16,7 +16,7 @@
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
 
-static char *dev_name = "/dev/video3";
+static char *dev_name = "/dev/video0";
 static int fd         = -1;
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -57,171 +57,11 @@ static void draw_MJPEG()
 	SDL_RWclose(buf_stream);
 }
 
-static int read_frame()
-{
-	struct v4l2_buffer buf;
-	memset(&buf, 0, sizeof(buf));
-	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	buf.memory = V4L2_MEMORY_MMAP;
-
-	if (ioctl(fd, VIDIOC_DQBUF, &buf) == -1) {
-		printf("ioctl dqbuf is wrong !!!\n");
-		switch (errno) {
-			case EAGAIN:
-				return 0;
-			case EIO:
-				/* Could ignore EIO, see spec. */
-				/* fall through */
-			default:
-				errno_exit("VIDIOC_DQBUF");
-		}
-	}
-
-	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV)
-		draw_YUV();
-	else if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG)
-		draw_MJPEG();
-
-	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	buf.memory = V4L2_MEMORY_MMAP;
-
-	if (ioctl(fd, VIDIOC_QBUF, &buf) == -1)
-		errno_exit("VIDIOC_QBUF");
-
-	return 1;
-}
 
 static int sdl_filter(void *userdata, SDL_Event *event)
 {
 	(void)userdata;
 	return event->type == SDL_QUIT;
-}
-
-static void mainloop(void)
-{
-	SDL_Event event;
-	for (;;) {
-		while (SDL_PollEvent(&event))
-			if (event.type == SDL_QUIT)
-				return;
-		for (;;) {
-			fd_set fds;
-			struct timeval tv;
-			int r;
-
-			FD_ZERO(&fds);
-			FD_SET(fd, &fds);
-
-			tv.tv_sec = 2;
-			tv.tv_usec = 0;
-
-			r = select(fd + 1, &fds, NULL, NULL, &tv);
-
-			if (-1 == r)
-			{
-				if (EINTR == errno)
-					continue;
-
-				errno_exit("select");
-			}
-
-			if (0 == r)
-			{
-				fprintf(stderr, "select timeout\n");
-				exit(EXIT_FAILURE);
-			}
-
-			if (read_frame())
-				break;
-			// EAGAIN - continue select loop.
-		}
-	}
-}
-
-static void stop_capturing(void)
-{
-	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	if (ioctl(fd, VIDIOC_STREAMOFF, &type) == -1)
-		errno_exit("VIDIOC_STREAMOFF");
-}
-
-static void start_capturing(void)
-{
-	struct v4l2_buffer buf;
-	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-
-	memset(&buf, 0, sizeof(buf));
-	buf.type = type;
-	buf.memory = V4L2_MEMORY_MMAP;
-	buf.index = 0;
-
-	if (ioctl(fd, VIDIOC_QBUF, &buf) == -1)
-		errno_exit("VIDIOC_QBUF ... !!!");
-
-	if (ioctl(fd, VIDIOC_STREAMON, &type) == -1)
-		errno_exit("VIDIOC_STREAMON");
-}
-
-static void uninit_device(void)
-{
-	if (munmap(buffer_start, length) == -1)
-		errno_exit("munmap");
-}
-
-static int init_view()
-{
-	if (SDL_Init(SDL_INIT_VIDEO)) {
-		printf("Unable to initialize SDL\n");
-		return -1;
-	}
-
-	if (SDL_CreateWindowAndRenderer(fmt.fmt.pix.width
-					, fmt.fmt.pix.height
-					, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL
-					, &window
-					, &renderer)) {
-		printf("SDL_CreateWindowAndRenderer failed: %s\n", SDL_GetError());
-		return -1;
-	}
-
-	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV) {
-		texture = SDL_CreateTexture(renderer
-						// YUY2 is also know as YUYV in SDL
-						, SDL_PIXELFORMAT_YUY2
-						, SDL_TEXTUREACCESS_STREAMING
-						, fmt.fmt.pix.width
-						, fmt.fmt.pix.height);
-		if (!texture) {
-			printf("SDL_CreateTexture failed: %s\n", SDL_GetError());
-			return -1;
-		}
-	} else if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG) {
-		if (!IMG_Init(IMG_INIT_JPG)) {
-			printf("Unable to initialize IMG\n");
-			return -1;
-		}
-	}
-
-	printf("Device: %s\nWidth: %d\nHeight: %d\nFramerate: %d\n"
-		, dev_name, fmt.fmt.pix.width, fmt.fmt.pix.height, fps);
-
-	return 0;
-}
-
-static void close_view()
-{
-	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV)
-		SDL_DestroyTexture(texture);
-
-	SDL_DestroyRenderer(renderer);
-
-	SDL_DestroyWindow(window);
-
-	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG)
-		IMG_Quit();
-
-	if (SDL_WasInit(SDL_INIT_VIDEO)) 
-		SDL_Quit();
 }
 
 static void init_mmap(void)
@@ -272,6 +112,19 @@ static void get_pixelformat()
 		}
 	}
 	printf("Using YUYV\n");
+}
+
+static void open_device(char* dev_name)
+{
+	if ((fd = open(dev_name, O_RDWR)) < 0) {
+		perror("Can't open device");
+		exit(1);
+	}
+}
+static void close_device(void)
+{
+	if (close (fd) == -1)
+		errno_exit("close");
 }
 
 static void init_device(void)
@@ -326,22 +179,190 @@ static void init_device(void)
 	init_mmap ();
 }
 
-static void close_device(void)
+static void uninit_device(void)
 {
-	if (close (fd) == -1)
-		errno_exit("close");
+	if (munmap(buffer_start, length) == -1)
+		errno_exit("munmap");
+}
+static int init_view()
+{
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("Unable to initialize SDL\n");
+		return -1;
+	}
+
+	if (SDL_CreateWindowAndRenderer(fmt.fmt.pix.width
+					, fmt.fmt.pix.height
+					, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL
+					, &window
+					, &renderer)) {
+		printf("SDL_CreateWindowAndRenderer failed: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV) {
+		texture = SDL_CreateTexture(renderer
+						// YUY2 is also know as YUYV in SDL
+						, SDL_PIXELFORMAT_YUY2
+						, SDL_TEXTUREACCESS_STREAMING
+						, fmt.fmt.pix.width
+						, fmt.fmt.pix.height);
+		if (!texture) {
+			printf("SDL_CreateTexture failed: %s\n", SDL_GetError());
+			return -1;
+		}
+	} else if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG) {
+		if (!IMG_Init(IMG_INIT_JPG)) {
+			printf("Unable to initialize IMG\n");
+			return -1;
+		}
+	}
+
+	printf("Device: %s\nWidth: %d\nHeight: %d\nFramerate: %d\n"
+		, dev_name, fmt.fmt.pix.width, fmt.fmt.pix.height, fps);
+
+	return 0;
 }
 
-static void open_device(char* dev_name)
+static void start_capturing(void)
 {
-	if ((fd = open(dev_name, O_RDWR)) < 0) {
-		perror("Can't open device");
-		exit(1);
+	struct v4l2_buffer buf;
+	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+	memset(&buf, 0, sizeof(buf));
+	buf.type = type;
+	buf.memory = V4L2_MEMORY_MMAP;
+	buf.index = 0;
+
+	if (ioctl(fd, VIDIOC_QBUF, &buf) == -1)
+		errno_exit("VIDIOC_QBUF ... !!!");
+
+	if (ioctl(fd, VIDIOC_STREAMON, &type) == -1)
+		errno_exit("VIDIOC_STREAMON");
+}
+
+static void stop_capturing(void)
+{
+	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	if (ioctl(fd, VIDIOC_STREAMOFF, &type) == -1)
+		errno_exit("VIDIOC_STREAMOFF");
+}
+
+static int read_frame()
+{
+	struct v4l2_buffer buf;
+	memset(&buf, 0, sizeof(buf));
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+
+	if (ioctl(fd, VIDIOC_DQBUF, &buf) == -1) {
+		printf("ioctl dqbuf is wrong !!!\n");
+		switch (errno) {
+			case EAGAIN:
+				return 0;
+			case EIO:
+				/* Could ignore EIO, see spec. */
+				/* fall through */
+			default:
+				errno_exit("VIDIOC_DQBUF");
+		}
 	}
+
+	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV)
+		draw_YUV();
+	else if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG)
+		draw_MJPEG();
+
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+
+	if (ioctl(fd, VIDIOC_QBUF, &buf) == -1)
+		errno_exit("VIDIOC_QBUF");
+
+	return 1;
+}
+
+static void mainloop(void)
+{
+	SDL_Event event;
+	for (;;) {
+		while (SDL_PollEvent(&event))
+			if (event.type == SDL_QUIT)
+				return;
+		for (;;) {
+			fd_set fds;
+			struct timeval tv;
+			int r;
+
+			FD_ZERO(&fds);
+			FD_SET(fd, &fds);
+
+			tv.tv_sec = 2;
+			tv.tv_usec = 0;
+
+			r = select(fd + 1, &fds, NULL, NULL, &tv);
+
+			if (-1 == r)
+			{
+				if (EINTR == errno)
+					continue;
+
+				errno_exit("select");
+			}
+
+			if (0 == r)
+			{
+				fprintf(stderr, "select timeout\n");
+				exit(EXIT_FAILURE);
+			}
+
+			if (read_frame())
+				break;
+			// EAGAIN - continue select loop.
+		}
+	}
+}
+
+static void close_view()
+{
+	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV)
+		SDL_DestroyTexture(texture);
+
+	SDL_DestroyRenderer(renderer);
+
+	SDL_DestroyWindow(window);
+
+	if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG)
+		IMG_Quit();
+
+	if (SDL_WasInit(SDL_INIT_VIDEO)) 
+		SDL_Quit();
+}
+
+static int help(char *prog_name)
+{
+	printf("Usage: %s [-d dev_name]\n", prog_name);
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
+	int opt;
+	while ((opt = getopt(argc, argv, "hd:")) != -1)
+	{
+		switch (opt) {
+			case 'd':
+				dev_name = optarg;
+				break;
+
+			case 'h':
+				return help(argv[0]);
+		}
+	}
+
+	if (argc == 1){
+		printf("Capturing from default device: %s\n", dev_name);
+	}
 	open_device(dev_name);
 	init_device();
 
