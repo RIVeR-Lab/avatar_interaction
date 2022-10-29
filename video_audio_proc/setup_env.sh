803e5a0b070321de82239ff39c6bf5a04bceabe3
xnuc3 <luo.rui@husky.neu.edu>
2022-10-29 15:15:16 -0400

    updated setup_env.sh with latest dependencies

--- video_audio_proc/setup_env.sh
+++ video_audio_proc/setup_env.sh
@@ -1,4 +1,5 @@
-sudo apt install -y libsdl2-dev libsdl2-image-dev libavcodec-dev libavformat-dev libswscale-dev
+#! /usr/bin/env bash
+sudo apt install -y libsdl2-dev libsdl2-image-dev libavcodec-dev libavformat-dev libswscale-dev libsdl2-ttf-dev
 
 echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib" >> ~/.bashrc
 
